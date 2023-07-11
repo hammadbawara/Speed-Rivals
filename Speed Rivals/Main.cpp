d9b05b8a473e0b8d6e4898250b9ead285cdd383e
Hammad Zafar Bawara <hammadbawara@gmail.com>
2023-07-11 20:02:25 +0500

    font and game entities position fixed

--- Speed Rivals/Main.cpp
+++ Speed Rivals/Main.cpp
@@ -61,24 +61,45 @@ public:
 
     GameEntity() {
     }
-    GameEntity(float yPosition, int road, Texture& texture) : yPosition(yPosition), texture(texture)
+    GameEntity(float yPosition, int road, Texture& texture, int track) : yPosition(yPosition), texture(texture)
     {
         sprite.setTexture(texture);
         sprite.setScale(1, 1);
-        switch (road)
-        {
-        case 1:
-            sprite.setPosition(0, yPosition);
-            break;
-        case 2:
-            sprite.setPosition(160, yPosition);
-            break;
-        case 3:
-            sprite.setPosition(310, yPosition);
-            break;
-        default:
-            break;
+
+        if (track == 1) {
+            switch (road)
+            {
+            case 1:
+                sprite.setPosition(0, yPosition);
+                break;
+            case 2:
+                sprite.setPosition(160, yPosition);
+                break;
+            case 3:
+                sprite.setPosition(310, yPosition);
+                break;
+            default:
+                break;
+            }
         }
+        else if (track == 2) {
+            switch (road)
+            {
+            case 1:
+                sprite.setPosition(550, yPosition);
+                break;
+            case 2:
+                sprite.setPosition(710, yPosition);
+                break;
+            case 3:
+                sprite.setPosition(860, yPosition);
+                break;
+            default:
+                break;
+            }
+        }
+        
+        
     }
 
     void draw(RenderWindow& window)
@@ -99,12 +120,12 @@ public:
 
 class Barrier : public GameEntity {
 public:
-    Barrier(float yPosition, int road, Texture& texture) : GameEntity(yPosition, road, texture) {}
+    Barrier(float yPosition, int road, Texture& texture, int track) : GameEntity(yPosition, road, texture, track) {}
 };
 
 class Boosters : public GameEntity {
 public:
-    Boosters(float yPosition, int road, Texture& texture) : GameEntity(yPosition, road, texture) {}
+    Boosters(float yPosition, int road, Texture& texture, int track) : GameEntity(yPosition, road, texture, track) {}
 };
 
 class Track {
@@ -113,18 +134,16 @@ class Track {
     int totalDistance;
 
     Sign roadSigns[14];
-    /*{Sign(140, 0), Sign(140, 150), Sign(140, 300), Sign(140, 450), Sign(140, 600), Sign(140, 750), Sign(140, 900),
-                          Sign(290, 0), Sign(290, 150), Sign(290, 300), Sign(290, 450), Sign(290, 600), Sign(290, 750), Sign(290, 900)};*/
-
-    int road;
+   
+    int track;
     Texture boosterTexture;
     Texture barrierTexture;
 public:
 
-    Track(int totalDistance, int r) {
+    Track(int totalDistance, int t) {
         this->totalDistance = totalDistance;
 
-        road = r;
+        track = t;
 
         noOfEntities = totalDistance / 300;
 
@@ -141,21 +160,21 @@ public:
             int random_entity = randomInt(0, 1);
             if (random_entity == 0)
             {
-                gameEntities[i] = Barrier(i * -300, random_road, barrierTexture);
+                gameEntities[i] = Barrier(i * -300, random_road, barrierTexture, track);
             }
             else if (random_entity == 1)
             {
-                gameEntities[i] = Boosters(i * -300, random_road, boosterTexture);
+                gameEntities[i] = Boosters(i * -300, random_road, boosterTexture, track);
             }
         }
 
-        if (road == 1) {
+        if (track == 1) {
             for (int i = 0; i < 7; i++) {
                 roadSigns[i].setSignValues(140, i * 150);
                 roadSigns[i+7].setSignValues(290, i * 150);
             }
         }
-        else if (road == 2) {
+        else if (track == 2) {
             for (int i = 0; i < 7; i++) {
 				roadSigns[i].setSignValues(690, i * 150.0);
 				roadSigns[i+7].setSignValues(840, i * 150.0);
@@ -214,7 +233,7 @@ public:
         carTexture.loadFromFile("./images/car.png");
         font.loadFromFile("./fonts/Roboto-Regular.ttf");
         speedText.setFont(font);
-        speedText.setPosition(10, 10);
+        speedText.setPosition(0, 0);
         speedText.setFillColor(Color::Red);
 
         carSprite.setTexture(carTexture);
@@ -222,6 +241,10 @@ public:
         speed = 0;
         topSpeed = 1000;
         acceleration = 0;
+    }
+
+    void setSpeedTextPosition(Vector2f position) {
+        speedText.setPosition(position.x, position.y);
     }
 
     void draw(RenderWindow& window)
@@ -296,8 +319,10 @@ int main()
     RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Car Game");
 
     Car car1(Vector2f(180, 600));
+    car1.setSpeedTextPosition(Vector2f(10, 10));
 
     Car car2(Vector2f(700, 600));
+    car2.setSpeedTextPosition(Vector2f(560, 10));
 
     Texture roadTexture;
     roadTexture.loadFromFile("images/road.png");
